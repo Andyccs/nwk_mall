81ccc40c1cfc6ee69e6a673256387d5cb5c5b06c
Dillon Amadeo <divestrato@dillonamadeo-3.local>
2014-12-18 17:53:41 +0700

    remove bug

--- backend/nwk/urls.py
+++ backend/nwk/urls.py
@@ -7,14 +7,17 @@ router.register(r'users', views.UserViewSet)
 router.register(r'groups', views.GroupViewSet)
 router.register(r'retails', views.RetailViewSet)
 router.register(r'promotions', views.PromotionViewSet)
-router.register(r'p_reductions', views.PromotionPriceReductionViewSet)
+router.register(r'p_reductions', views.PromotionReductionViewSet)
 router.register(r'p_discounts', views.PromotionDiscountViewSet)
 router.register(r'p_generals', views.PromotionGeneralViewSet)
 router.register(r'consumers', views.ConsumerGeneralViewSet)
 router.register(r'applications', views.ApplicationViewSet)
 
-urlpatterns = patterns('',
+urlpatterns = patterns(
+    '',
     url(r'^', include(router.urls)),
-    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    url(r'^api-auth/', include(
+        'rest_framework.urls',
+        namespace='rest_framework')),
     url(r'^register/$', views.register, name='detail'),
 )
